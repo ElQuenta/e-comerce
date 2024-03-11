c8fcd2bb6613beb0484ded8891541a994024b3c9
Miguel Quenta <angeloquenta@gmail.com>
2024-03-10 20:39:05 -0400

    conexion de html a base de datos

--- database.js
+++ database.js
@@ -20,27 +20,18 @@ class Database {
   }
 
   async connect() {
-    this.client = new Client({
-      user: 'root',
-      host: 'dpg-cnlv7ted3nmc73aqfagg-a.frankfurt-postgres.render.com',
-      database: 'ecomerce_p5fx',
-      password: 'pcUKq8GuBvYakVwb490odI8oOoR8rfwB',
-      port: 5432,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    });
-    await this.client.connect();
+      await this.client.connect();
   }
+
 
   async query(sql) {
     const res = await this.client.query(sql);
     return res.rows;
   }
 
-  async end() {
-    await this.client.end();
-  }
+  // async end() {
+  //   await this.client.end();
+  // }
 }
 
 // Exporta una instancia única del cliente de base de datos
--- server.js
+++ server.js
@@ -3,16 +3,36 @@ const app = express();
 
 // Conexión a la base de datos
 const db = require('./database');
+db.connect();
 
 app.use(express.static('public'));
 
 // Ruta para obtener categorías desde la base de datos
 app.get('/categorias', async (req, res) => {
   try {
-    await db.connect();
     const result = await db.query('SELECT * FROM categoria');
     console.log(result)
-    await db.end();
+    res.json(result);
+  } catch (error) {
+    console.error('Error al obtener categorías:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/catalogo-perros', async (req, res) => {
+  try {
+    const result = await db.query('SELECT nombre_producto, url FROM producto where id_categoria = 1');
+    console.log(result)
+    res.json(result);
+  } catch (error) {
+    console.error('Error al obtener categorías:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.get('/catalogo-gatos', async (req, res) => {
+  try {
+    const result = await db.query('SELECT nombre_producto, url FROM producto where id_categoria = 2');
+    console.log(result)
     res.json(result);
   } catch (error) {
     console.error('Error al obtener categorías:', error);
